ac1fa9305c8ba71461dba073b1eceb4f3f7bff58
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-08 23:59:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-function theBeatlesPlay(musicians, instruments){
-  var array = []
-  for (var i = 0; i<musicians.length; i++){
-    array.push (musicians[i]+" plays "+instruments[i]);
-  }
-  return array
-}
-
-function johnLennonFacts(array){
-  var i=0;
-  while (i<array.length){
-    array[i] = array[i]+"!!!";
-    i++;
-  }
-  return array
-}
-
-function iLoveTheBeatles(n){
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  do{array.push ("I love the Beatles!");
-  n++;
+  for (var i=0, i<musicians.length; i++){
+    array.push(musicians[i]+" plays "+instruments[i]);
   }
-  while (n<15);
   return array
 }
